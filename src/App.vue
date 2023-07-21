4e15f1ef32de6a7cecd580ba0297ecb40ffb1764
AliceBen <angelQin0704@163.com>
2023-07-21 23:21:48 +0800

    feat:架构

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { reqLogin } from './api/user'
+
+onMounted(() => {
+  reqLogin({ username: 'admin', password: '111' })
+})
+</script>
+
+<template>
+  <div>vite</div>
+</template>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//@ts-expect-error忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'virtual:svg-icons-register'
+import gloablComponent from './components/index'
+import '@/styles/index.scss'
+
+const app = createApp(App)
+app.mount('#app')
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.use(gloablComponent)
