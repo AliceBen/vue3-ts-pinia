30ebb8982192da1676535c523ff6d7ef7d6740f5
zhangqin.zq <zhangqin.zq@sunyur.com>
2023-08-07 16:48:41 +0800

    feat: layout布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,57 @@
 <template>
-  <div>首页</div>
+  <div class="home-wrap">
+    <div class="home-left">
+     
+    </div>
+    <div class="home-right">
+      <div class="home-nav"></div>
+      <div class="home-content">
+        <div class="content"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {}
+<script lang="ts" setup>
+
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import '../../styles/variable.scss';
+.home-wrap {
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  .home-left {
+    width: $home-left-width;
+    height: 100%;
+    background: black;
+  }
+  .home-right {
+    width: calc(100vw - $home-left-width);
+    height: 100%;
+    background: #ccc;
+  }
+  .home-nav {
+    width: calc(100vw - $home-left-width);
+    height: 50px;
+    border: 1px solid #fff;
+  }
+  .home-content {
+    position: relative;
+    width: calc(100vw - $home-left-width);
+    height: calc(100vh - 50px);
+    background: #fff;
+    padding: 20px;
+  }
+  .content {
+    position: absolute;
+    width: calc(100vw - $home-left-width - 40px);
+    height: 100px;
+    background: #ccc;
+  }
+}
+</style>
+
